31b131e66a58dff4d0f1577bea534386bdfc397d
mads østermark <madscramt@gmail.com>
2019-02-28 10:58:36 +0100

    Add project files.

---
+++ TSDBAOBAB_server/IMyFirstService.cs
@@ -0,0 +1,9 @@
+﻿using MagicOnion;
+
+namespace TSDBAOBAB_server {
+    public interface IMyFirstService : IService<IMyFirstService> {
+        // Return type must be `UnaryResult<T>` or `Task<UnaryResult<T>>`.
+        // If you can use C# 7.0 or newer, recommend to use `UnaryResult<T>`.
+        UnaryResult<int> SumAsync(int x, int y);
+    }
+}
---
+++ TSDBAOBAB_server/MyFirstService.cs
@@ -0,0 +1,13 @@
+﻿using MagicOnion;
+using MagicOnion.Server;
+
+namespace TSDBAOBAB_server {
+    public class MyFirstService : ServiceBase<IMyFirstService>, IMyFirstService {
+        // You can use async syntax directly.
+        public async UnaryResult<int> SumAsync(int x, int y) {
+            Logger.Debug($"Received:{x}, {y}");
+
+            return x + y;
+        }
+    }
+}
