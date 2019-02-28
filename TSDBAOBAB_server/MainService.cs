64b633950694b3dacf37ee1ccb59c52235921893
mads østermark <madscramt@gmail.com>
2019-02-28 11:01:07 +0100

    remaned some stuff

--- TSDBAOBAB_server/MainService.cs
+++ TSDBAOBAB_server/MainService.cs
@@ -2,7 +2,7 @@
 using MagicOnion.Server;
 
 namespace TSDBAOBAB_server {
-    public class MyFirstService : ServiceBase<IMyFirstService>, IMyFirstService {
+    public class MainService : ServiceBase<IMainService>, IMainService {
         // You can use async syntax directly.
         public async UnaryResult<int> SumAsync(int x, int y) {
             Logger.Debug($"Received:{x}, {y}");
